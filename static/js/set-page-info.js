fdbd42fd8bddff54d921786d4d67bbf73e61ecfc
yillkid <yillkid@gmail.com>
2021-11-09 10:07:27 +0800

    feat: revise issues.js

--- static/js/set-page-info.js
+++ static/js/set-page-info.js
@@ -14,7 +14,26 @@ function setPageInfo() {
     $("#nav-issues").addClass("active");
     
     // List issues
-    list_issues(getCookie("username"));
+    if (page === "issues.html") {
+      list_issues(getCookie("username"));
+    } else if (page === "issues-1.html") {
+      // Get task
+      var queryString = window.location.search;
+      var urlParams = new URLSearchParams(queryString);
+      var uuid = urlParams.get("uuid");
+      
+      // Set Task
+      setCookie("target", uuid, 1); 
+      
+      // TODO
+      set_content();
+    } else if (page == "issue-2.html") {
+      // TODO
+      set_content();
+
+    } else if (page === "issues-3.html") {
+      ticket_summary(getCookie("target"));
+    }
     
   } else if (page == "foot_print.html") {
     $("#nav-foot_print").addClass("active");
